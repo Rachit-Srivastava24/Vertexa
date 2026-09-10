ace1651f00feb0aa8ab0baa5324b898250e3cdb0
Rachit Srivastava <rachit24srivastava@gmail.com>
2026-09-11 00:01:19 +0530

    Turn

--- frontend/vite-project/src/pages/VideoMeet.jsx
+++ frontend/vite-project/src/pages/VideoMeet.jsx
@@ -19,7 +19,14 @@ const pendingCandidates = {};
 const pendingOffers = {};
 
 const peerConfigConnections = {
-    iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
+    iceServers: [
+        { urls: "stun:stun.l.google.com:19302" },
+        {
+            urls: "turn:openrelay.metered.ca:80",
+            username: "openrelayproject",
+            credential: "openrelayproject"
+        }
+    ]
 };
 
 export default function VideoMeetComponent() {
